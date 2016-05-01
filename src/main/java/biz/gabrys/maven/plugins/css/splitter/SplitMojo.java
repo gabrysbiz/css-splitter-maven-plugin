a4350d53af18b164cbb90954cab1baafe96e2048
agabrys <adam.gabrys@live.com>
2016-05-01 16:50:48 +0200

    #10 migrate from www.gabrys.biz to css-splitter-maven-plugin.projects.gabrys.biz

--- src/main/java/biz/gabrys/maven/plugins/css/splitter/SplitMojo.java
+++ src/main/java/biz/gabrys/maven/plugins/css/splitter/SplitMojo.java
@@ -1,14 +1,14 @@
-/*
+/**
  * CSS Splitter Maven Plugin
- * http://www.gabrys.biz/projects/css-splitter-maven-plugin/
+ * http://css-splitter-maven-plugin.projects.gabrys.biz/
  *
  * Copyright (c) 2015 Adam Gabryś
  *
  * This file is licensed under the BSD 3-Clause (the "License").
  * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *      https://raw.githubusercontent.com/gabrysbiz/css-splitter-maven-plugin/master/src/main/resources/license.txt
+ * You may obtain:
+ * - a copy of the License at project page
+ * - a template of the License at https://opensource.org/licenses/BSD-3-Clause
  */
 package biz.gabrys.maven.plugins.css.splitter;
 
