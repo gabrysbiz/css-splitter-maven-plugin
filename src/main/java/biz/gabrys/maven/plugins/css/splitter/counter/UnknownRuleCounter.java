861eb6ac7b19d61d4645af3a2874be0ac95758e4
agabrys <adam.gabrys@live.com>
2016-05-01 20:38:33 +0200

    #11 extend UnknownRuleCounter

--- src/main/java/biz/gabrys/maven/plugins/css/splitter/counter/UnknownRuleCounter.java
+++ src/main/java/biz/gabrys/maven/plugins/css/splitter/counter/UnknownRuleCounter.java
@@ -23,17 +23,40 @@ public class UnknownRuleCounter extends AbstractRuleCounter<UnknownRule> {
     @Override
     protected int count2(final UnknownRule rule) {
         final String code = rule.getCode();
-        final int value = code.split(";").length;
-
-        int correction = 0;
-        if (value > 1 && code.endsWith("}")) {
-            String tmp = code.substring(0, code.length() - 1);
-            tmp = tmp.trim();
-            if (tmp.endsWith(";")) {
-                correction = -1;
-            }
+        if (code.endsWith("}")) {
+            return countNestedRule(code);
         }
+        return countSimpleRule(code);
+    }
 
-        return value + correction;
+    private static int countSimpleRule(final String code) {
+        final String[] parts = code.split(";");
+        final int value = parts.length;
+        final String last = parts[value - 1];
+        if (last.trim().length() == 0) {
+            return value - 1;
+        }
+        return value;
+    }
+
+    private static int countNestedRule(final String code) {
+        String tmp = code.substring(0, code.length() - 1).trim();
+        if (!tmp.endsWith(";") && !tmp.endsWith("}")) {
+            tmp += ";";
+        }
+        final String[] parts = tmp.split("}");
+        int value = 0;
+        for (final String part : parts) {
+            value += countNestedPart(part);
+        }
+        return value;
+    }
+
+    private static int countNestedPart(final String part) {
+        String tmp = part.trim();
+        if (!tmp.endsWith(";")) {
+            tmp += ";";
+        }
+        return tmp.split(";").length;
     }
 }
